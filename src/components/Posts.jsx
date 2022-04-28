51bb03ccd3d5bc4c4399c85c4a7d171dc128bdb2
Abobaker Saeed <abobakersaeed123@gmail.com>
2022-04-28 15:18:07 -0500

    fetching data

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,7 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./Posts.css";
+export const Posts = () => {
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://hfxe5q.sse.codesandbox.io/messages")
+      .then((data) => {
+        setMessages(data.data);
+      })
+      .then(messages);
+  }, [messages]);
 
-export default function Posts() {
   return (
-    <div>Posts</div>
-  )
-}
+    <div className=" container">
+      {messages.map((val, key) => {
+        return (
+          <div className="Post card" key={key}>
+            <h1>
+              {val.title.length > 20
+                ? val.title.substring(0, 20) + "..."
+                : val.title}
+            </h1>
+            <h5>
+              By{" "}
+              {val.author.length > 100
+                ? val.author.substring(0, 100) + "..."
+                : val.author + " "}
+              on ({val.time})
+            </h5>
+            <h3>
+              {val.message.length > 100
+                ? val.message.substring(0, 100) + "..."
+                : val.message}
+            </h3>
+            <div className="buttons">
+              <button
+                className="btn btn-primary"
+              >
+                Read More
+              </button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
