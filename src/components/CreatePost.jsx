d3855a69955c2395a0acae7c5b9470eaee1bc966
Abobaker Saeed <abobakersaeed123@gmail.com>
2022-04-28 15:24:21 -0500

    Created form

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,5 +1,63 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import "./CreatePost.css";
 
-export default function CreatePost() {
-  return <div>CreatePost</div>;
-}
+export const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+     
+      axios.post("https://hfxe5q.sse.codesandbox.io/messages/new", {
+        title: title,
+        author: author,
+        message: message,
+      });
+     
+  };
+  return (
+    <div className="form">
+      <form>
+        <label htmlFor="title">Title: </label>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          placeholder="Title"
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <label htmlFor="author">Author: </label>
+        <input
+          type="text"
+          name="author"
+          id="author"
+          placeholder="Author"
+          onChange={(e) => {
+            setAuthor(e.target.value);
+          }}
+        />
+        <label htmlFor="message">Message: </label>
+        <input
+          type="text"
+          name="message"
+          placeholder="Message"
+          id="message"
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+        />
+        <button
+          type="submit"
+          className="btn btn-success"
+          onClick={handleSubmit}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
