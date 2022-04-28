fba27b6a44cd21941b98b723dd42cb918e45e297
Abobaker Saeed <abobakersaeed123@gmail.com>
2022-04-28 15:13:18 -0500

    Routes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,50 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CreatePost from "./components/CreatePost";
+import Posts from "./components/Posts.jsx";
+import SinglePost from "./components/SinglePost";
 
 function App() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="/">
-          Node Blog
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a
-                className="nav-link active"
-                aria-current="page"
-                href="/Createpost"
-              >
-                CreatePost
-              </a>
-            </li>
-          </ul>
+    <BrowserRouter>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            Node Blog
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a
+                  className="nav-link active"
+                  aria-current="page"
+                  href="/Createpost"
+                >
+                  CreatePost
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Posts />} />
+        <Route path="/CreatePost" element={<CreatePost />} />
+        <Route path="/SinglePost/:postId" element={<SinglePost />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
