94e5d571026cbcf2d55f2795b036db747efc18c0
Abobaker Saeed <abobakersaeed123@gmail.com>
2022-04-28 15:19:30 -0500

    displaying single posts

--- src/components/SinglePost.jsx
+++ src/components/SinglePost.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export default function SinglePost() {
+  let { postId } = useParams();
+  const [post, setPost] = useState({});
+  useEffect(() => {
+    axios
+      .get(`https://hfxe5q.sse.codesandbox.io/messages/${postId}`)
+      .then((data) => {
+        setPost(data.data);
+      });
+  }, []);
   return (
-    <div>SinglePost</div>
-  )
+    <div className="container2">
+      <div className="singlepost card">
+        <h1>{post.title}</h1>
+        <h5>
+          {post.author} | {post.time}
+        </h5>
+        <h2>{post.message}</h2>
+      </div>
+    </div>
+  );
 }
